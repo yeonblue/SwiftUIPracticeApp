d61af81808d4d5af6480a0ade3b6afab0ada58ec
최서연 <yeondublue@naver.com>
2022-06-20 20:47:55 +0900

    first commit

---
+++ SwiftUIPracticeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPracticeApp
+//
+//  Created by yeonBlue on 2022/06/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIPracticeApp/SwiftUIPracticeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeAppApp.swift
+//  SwiftUIPracticeApp
+//
+//  Created by yeonBlue on 2022/06/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
